fe4afe3ce003c836e33293609389ff13dae5e51b
Yunus Emre Yılmaz <118895936+yunusemreyl@users.noreply.github.com>
2026-03-27 11:26:25 +0300

    Delete test_nvidia.py

--- test_nvidia.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import os
-import runpy
-
-BASE = os.path.dirname(os.path.abspath(__file__))
-TARGET = os.path.join(BASE, "scripts", "tests", "test_nvidia.py")
-runpy.run_path(TARGET, run_name="__main__")
